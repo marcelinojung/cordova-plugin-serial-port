171db4bac0622822b93740b359a5610d0426b452
Marcelino Jung <brsmurf@naver.com>
2023-12-09 10:55:54 +0900

    Update SerialPortPlugin.js

--- www/SerialPortPlugin.js
+++ www/SerialPortPlugin.js
@@ -1,3 +1,4 @@
+cordova.define("cordova-plugin-serial-port.SerialPortPlugin", function(require, exports, module) {
 var exec = require('cordova/exec');
 
 
@@ -20,7 +21,12 @@ var SerialPort = {
     },
     setHex: function (isTrue) {
         exec(null, null, "SerialPortPlugin", "setHex", [isTrue]);
-    }
+    },
+    readcallback: function (success, error) {
+        exec(success, error, "SerialPortPlugin", "readcallback", []);
+    },
 };
 
 module.exports = SerialPort;
+
+});
